8e371ec2cd18756d664fae24c4e3a2f22a6c7052
Summer Choi <summer.choi2000@gmail.com>
2023-03-20 17:43:03 -0500

    python file added

---
+++ job_tracker.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+import PySimpleGUI as sg
+import pandas as pd
+
+sg.theme("Material1")
+
+current_dir = Path(__file__).parent if '__file__' in locals() else Path.cwd()
+EXCEL_FILE = current_dir / 'job_track.xlsx'
+df = pd.read_excel(EXCEL_FILE)
+
+layout = [
+    [sg.Text('Fill out the following fields:')],
+    [sg.Text('Employer', size=(15,1)), sg.InputText(key='Employer')],
+    [sg.Text('Position', size=(15,1)), sg.InputText(key='Position')],
+    [sg.Text('Status', size=(15,1)), sg.Combo(['Applied', 'Waiting for Interview', 'Rejected'], key='Status')],
+    [sg.Text('Link', size=(15,1)), sg.InputText(key='Link')],
+    [sg.Text('Date Applied', size=(15,1)), sg.InputText(key='Date Applied'),
+     sg.CalendarButton("Select Date", close_when_date_chosen=True, target="Date", format='%Y:%m:%d', size=(10,1))],
+    [sg.Text('Pay', size=(15,1)), sg.InputText(key='Pay')],
+    [sg.Submit(), sg.Button('Clear'), sg.Exit()]
+]
+
+window = sg.Window('Simple data entry form', layout)
+
+def clear_input():
+    for key in values:
+        window[key]('')
+    return None
+
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+    if event == 'Clear':
+        clear_input()
+    if event == 'Submit':
+        new_record = pd.DataFrame(values, index=[0])
+        df = pd.concat([df, new_record], ignore_index=True)
+        df.to_excel(EXCEL_FILE, index=False)
+        sg.popup('Saved!')
+        clear_input()
+window.close()
